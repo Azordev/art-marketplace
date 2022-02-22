155dcc9bff2a7d616d9ee650a45673833928b872
Israel-Laguan <katsukeinz@gmail.com>
2022-02-22 12:47:26 -0400

    centering the buttons with some improves

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,21 +7,19 @@ import { isAuthenticated } from "../utils/isAuthenticated";
 const notLoggedButtons = () => {
   if (!isAuthenticated()) {
     return (
-      <div className="mx-20 sm:mx-12 md:mx-12 xl:mx-12 container sm:col-span-2 sm:space-x-8 2xl:flex-col items-center justify-center xl:justify-between xl:px-10 2xl:px-0 2xl:justify-center sm:contents xl:contents 2xl:contents">
-        <div className="flex flex-col sm:flex-inle sm:space-x-8 2xl:flex-col contents">
-          <Link
-            to="/login"
-            className="flex space-x-4 sm:inline-flex items-end justify-center px-20 py-3.5 mb-5 md:inline-flex xl:inline-flex 2xl:mb-7 md:mb-7  text-xl font-semibold leading-6 text-white whitespace-nowrap bg-primary rounded-md shadow-button hover-scale tracking-add mt-5 xl:mt-5"
-          >
-            Iniciar Sesión
-          </Link>
-          <Link
-            to="/register"
-            className="flex space-x-4 sm:inline-flex shadow-button items-end justify-center px-20 py-3.5 mb-5 text-xl font-semibold leading-6  xl:mb-8 md:mb-7 text-white whitespace-no-wrap bg-tertiary hover-scale rounded-md md:inline-flex xl:inline-flex mt-5 md:mt-4 mt-5 xl:mt-5"
-          >
-            Regístrate
-          </Link>
-        </div>
+      <div className="justify-items-center sm:justify-items-start grid grid-cols-1 mx-auto sm:grid-cols-2 sm:col-span-2">
+        <Link
+          to="/login"
+          className="w-60 max-w-full sm:max-w-none text-center py-3.5 mb-5 md:mb-7 text-xl font-semibold leading-6 text-white whitespace-nowrap bg-primary rounded-md shadow-button hover-scale tracking-add mt-5 xl:mt-5"
+        >
+          Iniciar Sesión
+        </Link>
+        <Link
+          to="/register"
+          className="w-60 max-w-full sm:max-w-none text-center py-3.5 mb-5 md:mb-7 text-xl font-semibold leading-6 text-white whitespace-nowrap bg-tertiary rounded-md shadow-button hover-scale tracking-add mt-5 xl:mt-5"
+        >
+          Regístrate
+        </Link>
       </div>
     );
   }
