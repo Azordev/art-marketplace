c398539cec7e3cc919b9b552721f48e9b5ec69d6
Eghinner <eghinner@gmail.com>
2022-02-24 18:52:26 -0400

    react-paginate functional

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -122,18 +122,12 @@ const Products = () => {
 		}
 	};
 
-	// const [currentPage, setCurrentPage] = useState(1);
 	const [currentPosts, setCurrentPosts] = useState(null);
-	const [postsPerPage] = useState(7);
+	const [postsPerPage] = useState(10);
 
 	const [pageCount, setPageCount] = useState(0);
 	const [itemOffset, setItemOffset] = useState(0);
 
-	// const indexOfLastPost = currentPage * postsPerPage;
-	// const indexOfFirstPost = indexOfLastPost - postsPerPage;
-
-	// const pageNumbers = [...Array(Math.ceil(currentProducts.length / postsPerPage)+1).keys()].slice(1);
-	// var endOffset;
 	useEffect(() => {
 		const endOffset = itemOffset + postsPerPage;
 		setCurrentPosts(currentProducts.slice(itemOffset, endOffset))
@@ -142,7 +136,6 @@ const Products = () => {
 
 	const handlePageClick = (event) => {
 	  const newOffset = (event.selected * postsPerPage) % currentProducts.length;
-
 	  setItemOffset(newOffset);
 	};
 
@@ -150,8 +143,11 @@ const Products = () => {
 	return (
 		<>
 			<MainNav query={queryString.parse(search).q} />
+				{
+			//<div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row px-5 lg:px-20 pt-5 lg:pt-16 pb-20">
 
-			<div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row px-5 lg:px-20 pt-5 lg:pt-16 pb-20">
+				}
+				<div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row px-20 pt-5 lg:pt-16 pb-20">
 				<Filters
 					categoryClicked={categoryClicked}
 					categories={categories}
@@ -223,38 +219,26 @@ const Products = () => {
 
 							{
 								// currentPosts.length <= postsPerPage||
-								/*<Pagination
-									postsPerPage={postsPerPage}
-									totalPosts={currentProducts.length}
-									paginate={paginate}
-									currentPage={currentPage}
-									paginateFront={paginateFront}
-									paginateBack={paginateBack}
-								/>*/
 								<ReactPaginate
-								  onClick={()=>console.log('hey')}
-								  //forcePage={1}
 								  breakLabel="..."
 								  previousLabel="Anterior"
 								  nextLabel="Siguiente"
 								  onPageChange={handlePageClick}
+								  initialPage={1}
 								  marginPagesDisplayed={1}
 								  pageRangeDisplayed={1}
 								  pageCount={pageCount}
 								  renderOnZeroPageCount={null}
-								  //forcePage={itemOffset}
-								  breakClassName="bg-white px-2 md:px-4 py-2 cursor-pointer justify-self-end"
-								  containerClassName=' py-3 flex justify-center text-center'
+								  breakClassName="bg-white px-3 py-2 cursor-pointer justify-self-end"
+								  containerClassName='py-3 flex justify-center text-center'
+								  activeLinkClassName='bg-white'
 								  activeClassName='px-0 py-0 bg-white'
-								  activeLinkClassName='bg-white px-4 py-2'
-								  pageClassName='cursor-pointer bg-complementary mx-0.5 relative inline-flex items-center p-2 md:px-4 text-sm font-medium'
-								  pageLinkClassName=''
-								  disabledClassName=''
-								  disabledLinkClassName=' text-gray'
-								  nextClassName='truncate cursor-pointer ml-0.5 bg-complementary relative inline-flex items-center p-2 md:px-4 text-sm font-medium'
-								  previousLinkClassName='truncate'
-								  previousClassName='truncate cursor-pointer mr-0.5 bg-complementary relative inline-flex items-center p-2 md:px-4 text-sm font-medium'
-								  nextLinkClassName='truncate'
+								  pageLinkClassName='cursor-pointer px-4 py-3 bg-complementary mx-0.5 relative inline-flex items-center text-sm font-medium'
+								  previousLinkClassName='truncate cursor-pointer bg-complementary relative inline-flex items-center px-3 py-2 text-sm font-medium'
+								  nextLinkClassName='truncate cursor-pointer bg-complementary relative inline-flex items-center px-3 py-2 text-sm font-medium'
+								  nextClassName='truncate cursor-pointer mr-0.5 bg-complementary relative inline-flex items-center'
+								  previousClassName='truncate cursor-pointer ml-0.5 bg-complementary relative inline-flex items-center'
+								  disabledLinkClassName='pointer-events-none'
 								/>
 							}
 						</>
