a2c10a309da3e2663013b0d41dbe16b5d4f00463
Eghinner <eghinner@gmail.com>
2022-02-24 16:51:12 -0400

    🚧 react-paginate in work

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -18,6 +18,8 @@ import {
 import Pagination from "../components/Products/Pagination";
 import { getCategories } from "../actions/categoriasActions";
 
+import ReactPaginate from 'react-paginate';
+
 const Products = () => {
 	const { search } = useLocation();
 	const { push } = useHistory();
@@ -120,32 +122,36 @@ const Products = () => {
 		}
 	};
 
-	const [currentPage, setCurrentPage] = useState(1);
-	const [postsPerPage] = useState(10);
+	// const [currentPage, setCurrentPage] = useState(1);
+	const [currentPosts, setCurrentPosts] = useState(null);
+	const [postsPerPage] = useState(7);
 
-	const indexOfLastPost = currentPage * postsPerPage;
-	const indexOfFirstPost = indexOfLastPost - postsPerPage;
-	const currentPosts = currentProducts.slice(indexOfFirstPost, indexOfLastPost);
+	const [pageCount, setPageCount] = useState(0);
+	const [itemOffset, setItemOffset] = useState(0);
 
-	const paginateFront = e => {
-		e.preventDefault();
-		setCurrentPage(currentPage + 1)
-	}
-	const paginateBack = e => {
-		e.preventDefault();
-		setCurrentPage(currentPage - 1);
-	}
+	// const indexOfLastPost = currentPage * postsPerPage;
+	// const indexOfFirstPost = indexOfLastPost - postsPerPage;
 
-	const paginate = (e, pageNumber) => {
-		e.preventDefault();
-		setCurrentPage(pageNumber);
+	// const pageNumbers = [...Array(Math.ceil(currentProducts.length / postsPerPage)+1).keys()].slice(1);
+	// var endOffset;
+	useEffect(() => {
+		const endOffset = itemOffset + postsPerPage;
+		setCurrentPosts(currentProducts.slice(itemOffset, endOffset))
+		setPageCount(Math.ceil(currentProducts.length / postsPerPage))
+	}, [itemOffset, postsPerPage])
+
+	const handlePageClick = (event) => {
+	  const newOffset = (event.selected * postsPerPage) % currentProducts.length;
+
+	  setItemOffset(newOffset);
 	};
+
 
 	return (
 		<>
 			<MainNav query={queryString.parse(search).q} />
 
-			<div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row px-20 pt-5 lg:pt-16 pb-20">
+			<div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row px-5 lg:px-20 pt-5 lg:pt-16 pb-20">
 				<Filters
 					categoryClicked={categoryClicked}
 					categories={categories}
@@ -212,15 +218,40 @@ const Products = () => {
 								}
 							</div>
 
-							{currentProducts.length <= 10
-								||
-								<Pagination
+							{
+								// currentPosts.length <= postsPerPage||
+								/*<Pagination
 									postsPerPage={postsPerPage}
 									totalPosts={currentProducts.length}
 									paginate={paginate}
 									currentPage={currentPage}
 									paginateFront={paginateFront}
 									paginateBack={paginateBack}
+								/>*/
+								<ReactPaginate
+								  onClick={()=>console.log('hey')}
+								  //forcePage={1}
+								  breakLabel="..."
+								  previousLabel="Anterior"
+								  nextLabel="Siguiente"
+								  onPageChange={handlePageClick}
+								  marginPagesDisplayed={1}
+								  pageRangeDisplayed={1}
+								  pageCount={pageCount}
+								  renderOnZeroPageCount={null}
+								  //forcePage={itemOffset}
+								  breakClassName="bg-white px-2 md:px-4 py-2 cursor-pointer justify-self-end"
+								  containerClassName=' py-3 flex justify-center text-center'
+								  activeClassName='px-0 py-0 bg-white'
+								  activeLinkClassName='bg-white px-4 py-2'
+								  pageClassName='cursor-pointer bg-complementary mx-0.5 relative inline-flex items-center p-2 md:px-4 text-sm font-medium'
+								  pageLinkClassName=''
+								  disabledClassName=''
+								  disabledLinkClassName=' text-gray'
+								  nextClassName='truncate cursor-pointer ml-0.5 bg-complementary relative inline-flex items-center p-2 md:px-4 text-sm font-medium'
+								  previousLinkClassName='truncate'
+								  previousClassName='truncate cursor-pointer mr-0.5 bg-complementary relative inline-flex items-center p-2 md:px-4 text-sm font-medium'
+								  nextLinkClassName='truncate'
 								/>
 							}
 						</>
