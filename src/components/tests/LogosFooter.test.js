c5f40ba3260525edb4a1e4a221d3076d103baae0
Andrés Romaña <iam@andresromana.com>
2022-03-07 13:22:54 -0500

    removed unused files

--- src/components/tests/LogosFooter.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import LogosFooter from "../LogosFooter";
-
-it("renders correctly", () => {
-  const tree = renderer.create(<LogosFooter />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
