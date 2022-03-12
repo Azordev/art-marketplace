fff8906aa170b58df60a873bc7a19002d4051cc1
Alejandro Rodríguez López <arodriguezl9302@gmail.com>
2022-03-12 03:27:06 +0100

    feat: adjust the size of the buttons

--- src/components/isNotLogged.jsx
+++ src/components/isNotLogged.jsx
@@ -8,13 +8,13 @@ const notLoggedButtons = () => {
       <div className="grid grid-cols-1 justify-items-center sm:justify-items-start sm:grid-cols-2 sm:col-span-2">
         <Link
           to="/login"
-          className="w-60 max-w-full sm:max-w-none text-center py-3.5 mb-5 md:mb-7 text-xl font-semibold leading-6 text-white whitespace-nowrap bg-primary rounded-md shadow-button hover-scale tracking-add mt-5 xl:mt-5"
+          className="w-60 max-w-full sm:max-w-none text-center py-3.5 mb-5 md:mb-7 text-xl font-semibold leading-6 text-white whitespace-nowrap bg-primary rounded-md shadow-button hover-scale tracking-add mt-5 xl:mt-5 xl:h-14"
         >
           Iniciar Sesión
         </Link>
         <Link
           to="/register"
-          className="w-60 max-w-full sm:max-w-none text-center py-3.5 mb-5 md:mb-7 text-xl font-semibold leading-6 text-white whitespace-nowrap bg-tertiary rounded-md shadow-button hover-scale tracking-add mt-5 xl:mt-5"
+          className="w-60 max-w-full sm:max-w-none text-center py-3.5 mb-5 md:mb-7 text-xl font-semibold leading-6 text-white whitespace-nowrap bg-tertiary rounded-md shadow-button hover-scale tracking-add mt-5 xl:mt-5 xl:h-14"
         >
           Regístrate
         </Link>
